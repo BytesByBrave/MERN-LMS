a3eb5743b9a92fad7fbe2e644b676358d4401eb7
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-11-03 05:03:19 +0500

    feat: update CoursesList page to include search functionality, course list and integrate Footer component

--- client/src/components/students/CourseCard.jsx
+++ client/src/components/students/CourseCard.jsx
@@ -11,7 +11,7 @@ const CourseCard = ({course}) => {
         <Link className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg' to={'/course' + course._id} onClick={()=> scrollTo(0,0)} >
             <img className='w-full' src={course.courseThumbnail} alt="thumbnail" />
             <div className='p-3 text-left'>
-                <h3 className='text-base font-semibold'>{course.CourseTitle}</h3>
+                <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
                 <p className='text-gray-500'>{course.educator.name}</p>
                 <div className='flex items-center space-x-2'>
                     <p>{calculateRating(course)}</p>
