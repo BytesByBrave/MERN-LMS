89aa3845908a00a9265091d6cbc62d77288ddb19
bytesbybrave <bahadurali123470@gmail.com>
2026-01-27 22:14:12 +0500

    feat: implement Footer and Sidebar components, enhance Educator layout, and update MyCourses and StudentsEnrolled pages with headings

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MyCourses = () => {
     return (
         <div>
-        
+        <h1>My courses</h1>
         </div>
     )
 }
--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const StudentsEnrolled = () => {
     return (
         <div>
-        
+        <h1>Enrolled Students</h1>
         </div>
     )
 }
